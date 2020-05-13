89423817e927f9d96bd314cede52f0d0d0dac61b
Ryan McCahon <mccahonr@yahoo.com>
2020-05-13 01:37:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
 class Owner
   # code goes here
+
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+    @@all << self
+
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    Cat.all.select {|cat| @name == self }
+  end
 end
