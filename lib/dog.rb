6e59a088cc1c05ce118a0b014f95e24df815c863
Ryan McCahon <mccahonr@yahoo.com>
2020-05-13 01:53:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,10 +5,10 @@ class Dog
 
   @@all = []
 
-  def initialize(name, owner, mood)
+  def initialize(name, owner, mood = "nervous")
     @name = name
     @owner = owner
-    @mood = "nervous"
+    @mood = mood
     @@all << self
   end
 
